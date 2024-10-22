9fe88f9ed5ca931ee6d9c08f6ba81e8fc81de8f0
yutthaphichai08 <yutthaphichaikk@gmail.com>
2024-10-22 22:25:04 +0700

    install package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div style={{ textAlign: "center" }}>Hello World</div>;
+  return (
+    <div className="container" style={{ display: "flex" }}>
+      <div style={{ width: "50%" }}>1</div>
+      <div style={{ width: "50%" }}>2</div>
+    </div>
+  );
 }
